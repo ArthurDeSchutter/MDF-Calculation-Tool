68cd67100d8802b3ab4d6849ee360cc41c32d563
ArthurDeSchutter <deschutterarthur1@gmail.com>
2021-05-25 16:31:04 +0200

    added mvvm structure

---
+++ Model/PaymentMethod.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MDF_Calculation_Tool
+{
+    class PaymentMethod : INotifyPropertyChanged
+    {
+        public PaymentMethod(string name, int fixedRate, int variableRate)
+        {
+            Name = name;
+            FixedRate = fixedRate;
+            VariableRate = variableRate;
+        }
+
+        private string name;
+        private int fixedrate;
+        private int variablerate;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged("name");
+            }
+        }
+        public int FixedRate
+        {
+            get
+            {
+                return fixedrate;
+            }
+            set
+            {
+                fixedrate = value;
+                OnPropertyChanged("fixedrate");
+            }
+        }
+        public int VariableRate
+        {
+            get
+            {
+                return variablerate;
+            }
+            set
+            {
+                variablerate = value;
+                OnPropertyChanged("variablerate");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
